57b147db8e4e3244e3b6a63867e9183aea6962fd
Павел <raevsky3@gmail.com>
2021-08-20 23:21:53 +0300

    Adds styles import into jsx and fixes classNames

--- src/components/elements/header/header.jsx
+++ src/components/elements/header/header.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import styles from "./header.module.scss";
 import { AppRoute } from "../../../const";
 
 function Header() {
   return (
-    <header className="header">
-      <nav className="main_nav">
-        <div className="logo">
-          <Link to={AppRoute.ROOT} className="link_logo">
+    <header className={styles.header}>
+      <nav className={styles.main_nav}>
+        <div className={styles.logo}>
+          <Link to={AppRoute.ROOT} className={styles.link_logo}>
             <img
-              className="img_logo"
+              className={styles.img_logo}
               src="https://via.placeholder.com/150x150"
               width="150"
               height="150"
@@ -19,30 +20,30 @@ function Header() {
           </Link>
         </div>
 
-        <ul className="nav_list">
-          <li className="nav_item">
-            <Link to={AppRoute.ROOT} className="link_home">
+        <ul className={styles.nav_list}>
+          <li className={styles.nav_item}>
+            <Link to={AppRoute.ROOT} className={styles.link_home}>
               Home
             </Link>
           </li>
-          <li className="nav_item">
-            <Link to={AppRoute.SEND_MONEY_PAGE} className="link_send">
+          <li className={styles.nav_item}>
+            <Link to={AppRoute.SEND_MONEY_PAGE} className={styles.link_send}>
               Send Money
             </Link>
           </li>
-          <li className="nav_item">
-            <Link to={"#"} className="link_top_up">
+          <li className={styles.nav_item}>
+            <Link to={"#"} className={styles.link_top_up}>
               Top Up
             </Link>
           </li>
-          <li className="nav_item">
-            <Link to={"#"} className="link_accounts">
+          <li className={styles.nav_item}>
+            <Link to={"#"} className={styles.link_accounts}>
               Accounts
             </Link>
           </li>
         </ul>
 
-        <Link to={"#"} className="link_profile">
+        <Link to={"#"} className={styles.link_profile}>
           Profile
         </Link>
       </nav>
